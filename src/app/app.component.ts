5e84c93b10144cb0eed8fbe8ee57accf491a5bd8
Arrow85 <hipolite.vincent@gmail.com>
2019-01-09 15:11:56 +0100

    Final application commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'blog-angular';
+
+  posts = [
+    {
+      title: "Mon premier post",
+      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      loveIts: 0,
+      created_at: new Date()
+    },
+    {
+      title: "Mon deuxième post",
+      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      loveIts: 10,
+      created_at: new Date()
+    },
+    {
+      title: "Mon troisième post",
+      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      loveIts: -5,
+      created_at: new Date()
+    }
+  ];
 }
